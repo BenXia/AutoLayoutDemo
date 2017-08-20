4276a01f23163b569e3233863096e5ba8e4322e8
夏许强 <xiaxuqiang@changingedu.com>
2017-08-20 21:23:33 +0800

    添加单件宏

--- TestTemplateProject/Sources/Global/macrodef.h
+++ TestTemplateProject/Sources/Global/macrodef.h
@@ -2,12 +2,113 @@
 //  macrodef.h
 //  TestTemplateProject
 //
-//  Created by Ben on 15/7/16.
-//  Copyright (c) 2015年 Ben. All rights reserved.
+//  Created by Ben on 1/26/15.
+//  Copyright © 2017年 iOSStudio. All rights reserved.
 //
 
 #ifndef BN_macrodef_h
 #define BN_macrodef_h
+
+
+// 单件.h中声明
+#undef BN_DEC_SINGLETON
+#define BN_DEC_SINGLETON(classname) \
++ (instancetype)sharedInstance;\
++ (instancetype)shared##classname;
+
+
+// 单件.m中实现
+#undef BN_IMP_SINGLETON
+#if __has_feature(objc_arc)
+#define BN_IMP_SINGLETON(classname) \
+\
+static classname *shared##classname = nil;\
+\
++ (instancetype)sharedInstance {\
+\
+static dispatch_once_t onceToken;\
+dispatch_once(&onceToken, ^{\
+shared##classname = [[super allocWithZone:NULL] init];\
+});\
+return shared##classname;\
+} \
+\
++ (instancetype)shared##classname {\
+\
+return [self sharedInstance];\
+}\
+\
++ (instancetype)allocWithZone:(struct _NSZone *)zone {\
+\
+return [self sharedInstance];\
+}\
+\
+- (id)copyWithZone:(NSZone *)zone {\
+return self;\
+}\
+\
+- (id)mutableCopyWithZone:(NSZone *)zone\
+{\
+return self;\
+}
+
+#else
+#define BN_IMP_SINGLETON(classname) \
+\
+static classname *shared##classname = nil;\
+\
++ (instancetype)sharedInstance {\
+\
+static dispatch_once_t onceToken;\
+dispatch_once(&onceToken, ^{\
+shared##classname = [[super allocWithZone:NULL] init];\
+});\
+return shared##classname;\
+} \
+\
++ (instancetype)shared##classname {\
+\
+return [self sharedInstance];\
+}\
+\
++ (instancetype)allocWithZone:(struct _NSZone *)zone {\
+\
+return [self sharedInstance];\
+}\
+\
+- (id)copyWithZone:(NSZone *)zone {\
+return self;\
+}\
+\
+- (id)mutableCopyWithZone:(NSZone *)zone {\
+return self;\
+}\
+\
+- (oneway void)release {\
+}\
+\
+- (instancetype)autorelease {\
+    return self; \
+}\
+\
+- (instancetype)retain {\
+return self;\
+}\
+\
+- (NSUInteger)retainCount {\
+return NSUIntegerMax;\
+}
+
+#endif
+
+#ifndef NS_ENUM
+#define NS_ENUM(_type, _name) enum _name : _type _name; enum _name : _type
+#endif
+
+#define RUNLOOP_RUN_FOR_A_WHILE \
+{ \
+[[NSRunLoop currentRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.1]]; \
+}
 
 // 检查系统版本
 #define SYSTEM_VERSION_EQUAL_TO(v)                  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedSame)
